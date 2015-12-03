23980f2b6c0881f62b5bb46916beddde244ceaf0
Santiago Martin-Cleto <Santiago.Martin-Cleto@lbigroup.es>
2015-12-03 18:21:18 +0100

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,27 @@
+( function($){
+
+  var packlinkTest;
+
+  function PacklinkTest() {
+
+    //DOM
+    this.$pageHeader = $( '.page-header' );
+    this.$navigationToggleButton = $( '.page-header__navigation-toggle' );
+
+    this.bindEvents();
+
+  }
+
+
+  PacklinkTest.prototype.bindEvents = function() {
+    var self = this;
+
+    this.$navigationToggleButton.on( 'click', function(){
+      self.$pageHeader.toggleClass( 'page-header--expanded' );
+      console.log( 'click' );
+    } )
+  };
+
+  packlinkTest = new PacklinkTest;
+
+} )(jQuery)
